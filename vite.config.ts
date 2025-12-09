34b9e0c9c5fccbb3d941953301fe71680673c6aa
lijincheng <410637357@qq.com>
2025-12-09 23:57:46 +0800

    update: 全部回滚

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,23 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  server: {
-    port: 8085,
-    host: '127.0.0.1',
-    // --- 新增配置开始 ---
-    // 允许你的 frp 域名访问
-    allowedHosts: [
-      'pod.lordorange.top'
-    ],
-    // --- 新增配置结束 ---    
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8086',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
       },
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  }
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
