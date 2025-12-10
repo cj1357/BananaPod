240c55dc6cbef9e839f0854464ff60dbc7e4aef9
lijincheng <410637357@qq.com>
2025-12-10 21:54:23 +0800

    chore: 将默认语言切换为中文
    - 将默认语言从英文切换为中文。

--- App.tsx
+++ App.tsx
@@ -382,7 +382,7 @@ const App: React.FC = () => {
     const [editingElement, setEditingElement] = useState<{ id: string; text: string; } | null>(null);
     const [lassoPath, setLassoPath] = useState<Point[] | null>(null);
 
-    const [language, setLanguage] = useState<'en' | 'zho'>('en');
+    const [language, setLanguage] = useState<'en' | 'zho'>('zho');
     const [uiTheme, setUiTheme] = useState({ color: '#171717', opacity: 0.7 });
     const [buttonTheme, setButtonTheme] = useState({ color: '#374151', opacity: 0.8 });
     
